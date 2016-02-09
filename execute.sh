dce9ba8d0570fd79191606e08ab55e2180081c68
Dieter De Mesmaeker <dieter@datamind.org>
2016-02-09 13:15:52 +0100

    limit the CPU and nproc

--- execute.sh
+++ execute.sh
@@ -17,7 +17,7 @@ printf "Writing log file\n"
 
 # Start script that will look over the package_versions.json and execute the install.packages
 printf "Starting custom-r container and executing update_packages.R\n"
-docker run --rm -v "$PWD":/home/ec2-user -v \
+docker run --rm --cpu-period=50000 --cpu-quota=20000 --ulimit nproc=200 -v "$PWD":/home/ec2-user -v \
   "$PWD/libs":/usr/local/lib/R/site-library custom-r \
   R CMD BATCH update_packages.R
 
